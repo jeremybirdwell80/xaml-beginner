4779eb80af4239395bbc1fca394f3afb6b380557
jeremybirdwell80 <jeremybirdwell80@gmail.com>
2015-10-06 00:21:39 -0500

    Homework#4

--- RestaurantManager/RestaurantManager.UniversalWindows/App.xaml.cs
+++ RestaurantManager/RestaurantManager.UniversalWindows/App.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using RestaurantManager.Models;
 
 namespace RestaurantManager.UniversalWindows
 {
@@ -22,12 +23,14 @@ namespace RestaurantManager.UniversalWindows
     /// </summary>
     sealed partial class App : Application
     {
+        
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().
         /// </summary>
         public App()
         {
+            DataManager DataContext = new DataManager();
             this.InitializeComponent();
             this.Suspending += OnSuspending;
         }
